acdedaa995edfc31fc242fbef5afee029c4a89fa
JairCarrillo <113036574+JairCarrillo@users.noreply.github.com>
2023-09-07 08:39:50 -0500

    Add files via upload

---
+++ ejercicio2.js
@@ -0,0 +1,11 @@
+let Empleado1= "Jair";
+let Empleado2= "Diego";
+let Empleado3= "Juan";
+parseInt= Empleado1 = 2000000
+parseInt= Empleado2 = 3000000
+parseInt= Empleado2 = 4000000
+
+for (let Sueldo = 2000000; Sueldo < array.length; Sueldo++) {
+    const element = array Empleado1,
+}
+console console.log(sueldo);
